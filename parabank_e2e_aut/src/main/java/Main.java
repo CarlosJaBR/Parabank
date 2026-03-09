00ea560a672e5ba618182bbd1b0a2789e884b926
carlos javier <carlosjavier@MacBook-Air-de-carlos.local>
2026-03-09 09:39:41 -0500

    feat: add Main class

--- parabank_e2e_aut/src/main/java/Main.java
+++ parabank_e2e_aut/src/main/java/Main.java
@@ -8,5 +8,46 @@ public class Main {
         } else if (luzSemaforo.equals("Rojo")) {
             System.out.println("Parar");
         }
+
+        for(int i = 0; i <= 13;i+=3){
+            System.out.println(i); // Incremento definido de 3 en 3.
+        }
+        System.out.println("-----------");
+        int tabla = 2;
+        for(int i = 0; i < 10;i++){ // este incremento es de 1 en 1.
+            System.out.println("el resultado de "+ i +"x"+tabla + " = " + i*tabla);
+        }
+
+        System.out.println("------while-----");
+        int contador= 0;
+        while(contador<=10){
+            if((contador%2) == 0) {
+                System.out.println("Resultado de " + tabla + "x" + contador + " =" + tabla * contador);
+            }
+            contador++;
+        }
+        System.out.println("--Sucesión---");
+        int limite = 8;
+        int a = 0;
+        int b = 1;
+        for(int i = 1;i<=limite; i++){  // Para planeación en HU - Sprints.
+            System.out.println("Valor:" + a );
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+        int n= 10;
+        double sumatoria = 0;
+
+        System.out.println("---promedio---");
+        for(int i = 5; i<=n;i++){
+            sumatoria += i;
+        }
+        double promedio = sumatoria/5;
+        System.out.println("Promedio:" + promedio);
+
+
+
+
     }
 }
