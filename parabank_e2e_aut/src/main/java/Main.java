c9e6bbfc8992b43d50e54c1deeb1eb10196621ff
CarlosJaBR <carjabo12@gmail.com>
2026-03-16 07:24:22 -0500

    feat: add ejercicio

--- parabank_e2e_aut/src/main/java/Main.java
+++ parabank_e2e_aut/src/main/java/Main.java
@@ -3,89 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
         /*
-        String luzSemaforo = "Rojo";
-        if(luzSemaforo.equals("Verde")){
-            System.out.println("Avanzar");
-        } else if (luzSemaforo.equals("Amarillo")) {
-            System.out.println("Reducir velocidad");
-        } else if (luzSemaforo.equals("Rojo")) {
-            System.out.println("Parar");
-        }
-
-        for(int i = 0; i <= 13;i+=3){
-            System.out.println(i); // Incremento definido de 3 en 3.
-        }
-        System.out.println("-----------");
-        int tabla = 2;
-        for(int i = 0; i < 10;i++){ // este incremento es de 1 en 1.
-            System.out.println("el resultado de "+ i +"x"+tabla + " = " + i*tabla);
-        }
-
-        System.out.println("------while-----");
-        int contador= 0;
-        while(contador<=10){
-            if((contador%2) == 0) {
-                System.out.println("Resultado de " + tabla + "x" + contador + " =" + tabla * contador);
-            }
-            contador++;
-        }
-        System.out.println("--Sucesión---");
-        int limite = 8;
-        int a = 0;
-        int b = 1;
-        for(int i = 1;i<=limite; i++){  // Para planeación en HU - Sprints.
-            System.out.println("Valor:" + a );
-            int siguiente = a + b;
-            a = b;
-            b = siguiente;
-        }
-        int n= 10;
-        double sumatoria = 0;
-
-        System.out.println("---promedio---");
-        for(int i = 5; i<=n;i++){
-            sumatoria += i;
-        }
-        double promedio = sumatoria/5;
-        System.out.println("Promedio:" + promedio);
-
-
-        System.out.println("Creación cuenta");
-        String nombre = "Carlos Bolaños";
-        String correo = "Carlos@gmail.com";
-        String DNI = "1001010";
-        int monto = 40000;
-        int edad = 18;
-
-        if(edad >= 18 && monto >= 50000){
-            Cuenta miCuenta = new Cuenta(nombre,correo,DNI,monto,edad);
-            System.out.println("Cuenta Creada Exitosamente " + miCuenta.getNumeroCuenta());
-        }
-
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
-        cars.remove(1);
-        System.out.println(cars);
-        System.out.println(cars.size());
-        //cars.clear();
-        //System.out.println(cars);
-        cars.add("Mercedes");
-        cars.add("Kia");
-        for(int j = 0; j < cars.size(); j++){
-            System.out.println(cars.get(j));
-        }
-        ArrayList<String> myCars = new ArrayList<String>();
-        for(int i = 0; i< cars.size();i++){
-            myCars.add(cars.get(i));
-        }
-        System.out.println(myCars);
-
-
         System.out.println("Quiz");
         ArrayList<Double> compras = new ArrayList<Double>();
         Scanner scan = new Scanner(System.in);
@@ -106,7 +25,7 @@ public class Main {
         System.out.println("ventas totales: " + total);
         System.out.println("Ventas mayores o iguales a 100: " + mayores100);
         System.out.println("Ventanas menores a 100: " + menores100);
-        */
+
 
         Cliente cliente = new Cliente("Carlos",1500000.0);
         Cliente ana = new Cliente("Ana",900000.0);
@@ -116,6 +35,26 @@ public class Main {
         double comision = calculo.bonificacion(ana.getTotalCompras());
         Vendedor vendedor = new Vendedor("Juanito");
         vendedor.infoSalario(comisionJuan);
+
+        */
+
+        Scanner sca = new Scanner(System.in);
+        int sumaTotal = 0;
+        int tamanoArray = 5;
+        int contador = 0;
+        int[] numeros = new int[tamanoArray];
+        while(contador<tamanoArray){
+            System.out.println("Ingrese número: " +(1+contador));
+            numeros[contador] = sca.nextInt();
+            contador++;
+        }
+        contador = 0;
+        while(contador<tamanoArray){
+            sumaTotal+=numeros[contador];
+            contador++;
+        }
+        System.out.println("Suma total: " + sumaTotal);
+
 
     }
 }
