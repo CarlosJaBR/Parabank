70f3bd389326c333f16291e50f8e6050af2be178
CarlosJaBR <carjabo12@gmail.com>
2026-03-18 08:00:01 -0500

    feat: add new Main class

---
+++ parabank_e2e_aut/src/main/java/salario/Main.java
@@ -0,0 +1,7 @@
+package salario;
+
+public class Main {
+    public static void main(String[] args){
+        EmpleadoTCompleto emp1 = new EmpleadoTCompleto("Jhon","Doe",1,21000000.0,"Indefinido",40000.0);
+    }
+}
